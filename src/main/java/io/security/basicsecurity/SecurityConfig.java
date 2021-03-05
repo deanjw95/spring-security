f73dac9584df55ed693573556320eb2233f8cc7b
Byun Jeongwook <deanjw95@naver.com>
2021-03-05 15:35:33 +0900

    1-11 권한 설정과 표현식, 1-12 예외 처리 및 요청 필터

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -2,16 +2,23 @@ package io.security.basicsecurity;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -28,12 +35,41 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     UserDetailsService userDetailsService;
 
     @Override
+    // 사용자 생성하고 권한 부여하는 메소드
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // 메모리 방식으로 생성, password 앞 prefix는 암호화 방식
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1111").roles("USER");
+        auth.inMemoryAuthentication().withUser("sys").password("{noop}1111").roles("SYS","USER");
+        auth.inMemoryAuthentication().withUser("admin").password("{noop}1111").roles("ADMIN","SYS","USER");
+
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 // 요청에 대한 보안검사
                 .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 // 어느 요청에나 인증받도록 한다
                 .anyRequest().authenticated();
+        http
+                // 인증, 인가 예외처리
+                .exceptionHandling()
+//                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+//                    @Override
+//                    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+//                        httpServletResponse.sendRedirect("/login");
+//                    }
+//                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    @Override
+                    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+                        httpServletResponse.sendRedirect("/denied");
+                    }
+                });
         http
                 .formLogin()
 //                .loginPage("/loginPage")
@@ -49,6 +85,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                        httpServletResponse.sendRedirect("/");
 //                    }
 //                })
+                .successHandler(new AuthenticationSuccessHandler() {
+                    @Override
+                    // 인증에 성공하면 곧바로 그 전 요청주소로 돌아가도록 설정
+                    public void onAuthenticationSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
+                        RequestCache requestCache = new HttpSessionRequestCache();
+                        // 사용자가 원래 가고자 했던 요청 정보가 session에서 꺼내와 저장
+                        SavedRequest savedRequest = requestCache.getRequest(httpServletRequest, httpServletResponse);
+                        String redirectUrl = savedRequest.getRedirectUrl();
+                        httpServletResponse.sendRedirect(redirectUrl);
+                    }
+                })
 //                .failureHandler(new AuthenticationFailureHandler() {
 //                    @Override
 //                    public void onAuthenticationFailure(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
@@ -85,14 +132,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .tokenValiditySeconds(3600)
                 // 인증 시 user 계정을 조회하는 기능
 //                .userDetailsService(userDetailsService);
-        http
-                // 동시 세션 제어
-                .sessionManagement()
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(true);
-        http
-                // 세션 고정 보호
-                .sessionManagement()
-                .sessionFixation().changeSessionId();
+//        http
+//                // 동시 세션 제어
+//                .sessionManagement()
+//                .maximumSessions(1)
+//                .maxSessionsPreventsLogin(true);
+//        http
+//                // 세션 고정 보호
+//                .sessionManagement()
+//                .sessionFixation().changeSessionId();
     }
 }
